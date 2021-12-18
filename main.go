45dd6678f6d2a48a95591e65ebdb987e1ed3fd88
canergulay <caner.gulay@hotmail.com>
2021-12-18 23:28:18 +0300

    main explained

--- main.go
+++ main.go
@@ -9,8 +9,14 @@ import (
 )
 
 func main() {
+	//LETS GIVE A TRY TO IT
+
+	//WE WILL INITIALISE OUR LOGRUSTER WITH AN INTERVAL 10 SECONDS
+	//WHICH MEANS IT WILL RECREATE A BRAND NEW LOG FILE IN A INTERVAL OF 10 SECONDS
+	//IT WILL ALSO SAVE ALL OF THOSE STUFF IN /LOGS FOLDER AS WE GIVE IT SO.
 	logruster := logruster.New(int(time.Second*10), "logs")
 
+	//LETS LOG SOME DUMMY DATA AND SEE WHAT HAPPENS
 	for i := 0; i < 10000; i++ {
 		time.Sleep(time.Second * 2)
 		info := fmt.Sprintf("Logrus has logged for %dth time ! ", i)
